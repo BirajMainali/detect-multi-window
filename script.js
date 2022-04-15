359ef6d2f22ad39efba1e94dee2bfb63272ea8a5
Biraj Mainali <biraj.prathamit@gmail.com>
2022-04-15 20:41:22 +0545

    feat: detect multiple browser windows

---
+++ script.js
@@ -0,0 +1,16 @@
+const INITIAL_BOARDCAST = "WINDOW_OPENED";
+const CONFIRMATION_BOARDCAST = "SECOND_WINDOW_ALREADY_EXIST";
+const imageElem = document.getElementById('swag');
+const boardcast = new BroadcastChannel("cruderkat_channel");
+boardcast.onmessage = (e) => {
+    if (e.data === INITIAL_BOARDCAST) {
+        boardcast.postMessage(CONFIRMATION_BOARDCAST);
+    }
+    if (e.data = CONFIRMATION_BOARDCAST) {
+        alert("Are you sure you want to open a new window?");
+        imageElem.src = "https://c.tenor.com/pb06GKyRor0AAAAd/justin-timberlake-stare.gif";
+        document.title = "LOL.........";
+    }
+};
+boardcast.postMessage(INITIAL_BOARDCAST);
+
